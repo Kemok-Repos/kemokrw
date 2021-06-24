89993f4be80d55674f46b02c7010e792f8e01f08
Kevin Mazariegos <kevin@kemok.io>
2021-06-24 13:28:15 -0600

    Cambio de dependencias

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     install_requires=[
-        'pandas==1.2.4',
-        'psycopg2==2.8.6',
-        'pymssql==2.2.1',
-        'SQLAlchemy==1.4.16'
+        'pandas>=1.2.4',
+        'psycopg2-binary>=2.8.6',
+        'pymssql>=2.2.1',
+        'SQLAlchemy>=1.3.24'
     ]
 )
